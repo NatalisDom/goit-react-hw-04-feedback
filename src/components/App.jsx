10829c4dd18de2403295890e724dbc44c8e280d1
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-03-30 16:42:39 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,60 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Statistics } from 'components/statistics/Statistics';
 import { Section } from 'components/section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Notification } from 'components/notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const base = { good, neutral, bad };
+
+  const clickOfBtn = ({ target: { name } }) => {
+    if (name === 'good') {
+      setGood(prev => (prev += 1));
+    } else if (name === 'neutral') {
+      setNeutral(prev => (prev += 1));
+    } else if (name === 'bad') {
+      setBad(prev => (prev += 1));
+    }
   };
 
-  hadleIncrement = evt => {
-    const { name } = evt.target;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
+  const countFeedback = () => {
+    const { good, neutral, bad } = base;
+    return good + neutral + bad;
   };
 
-  countTotalFeedback() {
-    return Object.values(this.state).reduce((acc, item) => {
-      return acc + item;
-    }, 0);
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countFeedback()) * 100);
+  };
 
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    return Math.round((good / this.countTotalFeedback()) * 100);
-  }
+  const filled = () => {
+    return Object.values(base).some(value => value > 0);
+  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.hadleIncrement}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(base)}
+          onLeaveFeedback={clickOfBtn}          
+        />
+      </Section>
+      <Section title="Statistics">
+        {filled() ? (
+          <Statistics
+            good={base.good}
+            neutral={base.neutral}                        
+            bad={base.bad}
+            total={countFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {good === 0 && neutral === 0 && bad === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </>
+  );
+};
